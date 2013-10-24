f9522eb064314b44af60c22fa3a49cbdcb295775
Zara Peisker <zarapeisker@gmail.com>
2013-10-23 17:03:49 -0700

    connected app to MySQL database

--- racetrack/grails-app/conf/DataSource.groovy
+++ racetrack/grails-app/conf/DataSource.groovy
@@ -1,43 +1,44 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "grails"
+    password = "server"
 }
 hibernate {
     cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.use_query_cache = true
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
         }
-    }
+        //update saved the data, create-drop drops the data each time you restart, create leaves the tables in place but deletes data on shutdown
+    }// end development
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
         }
-    }
+    }//end test
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
-    }
-}
+            url = "jdbc:mysql://localhost:3306/racetrack_dev?autoreconnect=true"
+            //pooled = true
+            //properties {
+              // maxActive = -1
+               //minEvictableIdleTimeMillis=1800000
+               //timeBetweenEvictionRunsMillis=1800000
+               //numTestsPerEvictionRun=3
+               //testOnBorrow=true
+               //testWhileIdle=true
+               //testOnReturn=true
+               //validationQuery="SELECT 1"
+            //}
+        }//end dataSource
+    }//end production
+}// end environ
